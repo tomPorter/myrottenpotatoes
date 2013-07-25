2f82cd763dba1a8f22b06a81d7da15c431a38af9
Tom Porter <thomas.porter@acm.org>
2013-07-25 09:56:49 -0700

    First route change

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,9 @@ Myrottenpotatoes::Application.routes.draw do
   # Sample resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
 
+  resources :movies
+    root :to => redirect('/movies')
+
   # Sample resource route with options:
   #   resources :products do
   #     member do
